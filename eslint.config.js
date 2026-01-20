6f5c37b8975ac27f357b638d5716b68db4312d8f
Raphael Otávio <154235078+Phaaek013@users.noreply.github.com>
2026-01-20 00:56:19 +0000

    feat: profissionais (lista + filtros) (#4)

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-alert': 'error',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
     },
   },
 ])
